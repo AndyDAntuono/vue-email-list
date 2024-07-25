2be3c3a85e00f7173a0de11c92623311171d23aa
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-25 21:37:30 +0200

    [UPD] updated index.html, readme.md, script.js files

--- README.md
+++ README.md
@@ -13,3 +13,7 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 - creo il metodo makeTenRandomMails a cui associo l'http axios e a sua volta lo associo all'endpoint https://flynn.boolean.careers/exercises/api/random/mail. Ciò permette di creare un indirizzo email randomico;
 - imposto un console log all'http e all'endpoint per assicurarmi che tutto fili liscio;
 - stampo l'indirizzo email nel file html;
+- modifico il vue.js per crea una proprità dentro alla sezione data per creare un array vuoto che chiamerò random_emails;
+- creo il metodo generateEmails il cui compito è riempire l'array vuoto random_emails con un ciclo di 10 iterazioni, le quali vengono prodotte dall'hettp axios e l'end-point fornitomi dalla consegna;
+- creo un bottone a cui conferisco la direttiva @click, la quale a sua volta richiama il metodo generateEmails
+- nel div ul inserisco un v-if in cui specifico che la lunghezza dell'array random_emails DEVE essere equivalente a 10 indirizzi e-mail, altrimenti non appare proprio
--- js/script.js
+++ js/script.js
@@ -3,58 +3,20 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            email: null
+            random_emails: []
             /*
-            emails: [
-                {
-                    //1
-                    text: "volareohoh@gmail.com"
-                },
-                {
-                    //2
-                    text: "javon33@gmail.com"
-                },
-                {
-                    //3
-                    text: "gunner97@outlook.com"
-                },
-                {
-                    //4
-                    text: "Kamren82@outlook.com"
-                },
-                {
-                    //5
-                    text: "Ephraim_Dickens-Abernathy31@yahoo.com"
-                },
-                {
-                    //6
-                    text: "Roxane14@yahoo.com"
-                },
-                {
-                    //7
-                    text: "Laney_Dicki96@aol.com"
-                },
-                {
-                    //8
-                    text: "Magnus.Hyatt19@aol.com"
-                },
-                {
-                    //9
-                    text: "Eulah.Beahan@yandex.com"
-                },
-                {
-                    //10
-                    text: "Missouri_Paucek@yandex.com"
-                },
-                
-            ],
+            email: null
             */
         }
     },
+    /*
     created() {
         this.makeTenRandomMails();
+        
     },
+    */
     methods: {
+        /*
         makeTenRandomMails() {
             //axios è l'http e 'https://flynn.boolean.careers/exercises/api/random/mail' è l'endpoint
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
@@ -65,5 +27,14 @@ createApp({
                 this.email = response.data.response
             });
         }
+        */
+       generateEmails() {
+        this.random_emails = [];
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                this.random_emails.push(response.data.response)
+            });
+        }
+       }
     }
 }).mount('#app');
